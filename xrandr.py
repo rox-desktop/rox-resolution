4e51d439b89b251b587b9d96bf331232319cab98
Tony Houghton <h@realh.co.uk>
2008-11-18 21:11:54 +0000

    Works with xrandr 1.2.
    git-svn-id: svn://svn.code.sf.net/p/rox/svn/trunk/config/Resolution@5291 66de3db3-b00d-0410-b41b-f4738ad19bea

--- xrandr.py
+++ xrandr.py
@@ -1,4 +1,5 @@
 import os, popen2, sys
+import rox
 
 paths = os.environ.get('PATH', '/bin:/usr/bin').split(':')
 paths += ['/sbin', '/usr/sbin', '/usr/local/sbin']
@@ -33,17 +34,46 @@ class Setting:
 	def __str__(self):
 		return '%s x %s' % (self.width, self.height)
 
+class NewSetting(Setting):
+	def __init__(self, line, phy_width, phy_height):
+		bits = [b for b in line.split() if b]
+		self.n = -1
+		self.width, self.height = bits[0].split('x')
+		self.width = int(self.width)
+		self.height = int(self.height)
+		self.phy_width = phy_width
+		self.phy_height = phy_height
+		self.res = []
+		self.current_r = None
+		for r in bits[1:]:
+			if r.endswith('*'):
+				self.current_r = float(r[:-1])
+				self.res.append(self.current_r)
+			else:
+				self.res.append(float(r))
+	
 def get_settings():
 	cout, cin = popen2.popen2([xrandr])
 	cin.close()
 	settings = []
 	current = None
+	phy_width = 0
+	phy_height = 0
 	for line in cout:
-		if line[0] in ' *' and ' x ' in line:
+		line = line.rstrip()
+		if 'mm x ' in line and line.endswith('mm'):
+			data = line.rsplit(' ', 3)
+			if data[1].endswith('mm') and data[3].endswith('mm'):
+				phy_width = data[1]
+				phy_height = data[3]
+		elif line[0] in ' *' and 'x' in line:
 			try:
-				setting = Setting(line[1:].strip())
+				if ' x ' in line:
+					setting = Setting(line[1:])
+				else:
+					setting = NewSetting(line, phy_width, phy_height)
 				settings.append(setting)
-				if line[0] == '*':
+				if '*' in line:
 					current = setting
 			except Exception, ex:
 				print >>sys.stderr, "Failed to parse line '%s':\n%s" % \
@@ -52,7 +82,8 @@ def get_settings():
 	return (current, settings)
 
 def set_mode(setting):
-	cerr, cin = popen2.popen4([xrandr, '-s', str(setting.n)])
+	cerr, cin = popen2.popen4([xrandr, '-s', '%dx%d' %
+		(setting.width, setting.height)])
 	cin.close()
 	errors = cerr.read()
 	if errors:
