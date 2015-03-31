ab1efe82d1f21e36730a254cc9097aa5d84f5ff8
Dennis Tomas <den.t@gmx.de>
2015-03-31 11:51:24 +0200

    Read layout from xrandr args setting.

--- xrandr.py
+++ xrandr.py
@@ -16,43 +16,13 @@ else:
 		  'system. Try upgrading your X server.'))
 
 class Setting:
-	def __init__(self, output, line):
-		bits = [b for b in line.split() if b]
-		self.output = output
-		self.n = int(bits[0])
-		self.width = int(bits[1])
-		self.height = int(bits[3])
-		self.phy_width = bits[5]
-		self.phy_height = bits[7]
-		self.enabled = self.phy_width != 0
-		self.res = []
-		self.other_output = None
-		self.direction = 'same-as'
-		self.current_r = None
-		for r in bits[9:]:
-			if r.startswith('*'):
-				self.current_r = int(r[1:])
-				self.res.append(self.current_r)
-			else:
-				self.res.append(int(r))
-	
-	def __str__(self):
-		return '%s x %s' % (self.width, self.height)
-
-class NewSetting(Setting):
-	def __init__(self, output, line, phy_width, phy_height):
+	def __init__(self, line):
 		bits = [b for b in line.split() if b]
 		self.n = -1
-		self.output = output
 		self.width, self.height = bits[0].split('x')
 		self.width = int(self.width)
 		self.height = int(self.height)
-		self.phy_width = phy_width
-		self.phy_height = phy_height
-		self.enabled = self.phy_width != 0
 		self.res = []
-		self.other_output = None
-		self.direction = 'same-as'
 		self.current_r = None
 		for r in bits[1:]:
 			try:
@@ -63,15 +33,27 @@ class NewSetting(Setting):
 				self.current_r = res
 			self.res.append(res)
 	
-def get_settings():
+	def __str__(self):
+		return '%s x %s' % (self.width, self.height)
+
+class Settings(list):
+
+	def __init__(self, output_index, output, phy_width, phy_height):
+		self.output = output
+		self.output_index = output_index
+		self.phy_width = phy_width
+		self.phy_height = phy_height
+		self.current = None
+		self.enabled = self.phy_width != 0
+		self.direction = 'same-as'
+		self.other_output = None
+
+def get_settings(xrandr_args):
 	cout, cin = popen2.popen2([xrandr])
 	cin.close()
-	settings = defaultdict(list)
-	current = {}
-	phy_width = 0
-	phy_height = 0
+	settings = {}
 	output = ''
-	i = -1
+	i = 0
 	for line in cout:
 		line = line.rstrip()
 		if 'connected' in line:
@@ -85,39 +67,59 @@ def get_settings():
 					phy_width = data[1]
 					phy_height = data[3]
 			output = line.split(' ')[0]
+			settings[output] = Settings(i, output, phy_width, phy_height)
 			i += 1
 		elif line[0] in ' *' and 'x' in line:
 			try:
-				if ' x ' in line:
-					setting = Setting(output, line[1:])
-				else:
-					setting = NewSetting(output, line, phy_width, phy_height)
-				settings[(i, output)].append(setting)
+				setting = Setting(line)
+				settings[output].append(setting)
 				if '*' in line:
-					current[output] = setting
+					settings[output].current = setting
 			except Exception, ex:
 				print >>sys.stderr, "Failed to parse line '%s':\n%s" % \
 					(line.strip(), str(ex))
 	cout.close()
-	return (current, settings)
 
-def settings_to_args(settings):
+	it = iter(xrandr_args.split(' '))
+
+	output = ''
+	while True:
+		try:
+			arg = it.next()
+		except StopIteration:
+			break
+		if arg == '--output':
+			try:
+				output = it.next()
+			except StopIteration:
+				# Invalid args: --output not followed by output name.
+				break
+		elif arg in {'--same-as', '--left-of', '--right-of', '--above', '--below'}:
+			settings[output].direction = arg[2:]
+			try:
+				settings[output].other_output = it.next()
+			except StopIteration:
+				# Invalid args: direction not followed by output name.
+				break
+	return settings
+
+def settings_to_args(output2settings):
 	args = []
-	for setting in settings:
-		args += ['--output', setting.output]
-		if setting.enabled:
-			args += ['--mode', '%dx%d' % (setting.width, setting.height)]
+	for output, settings in output2settings.iteritems():
+		args += ['--output', output]
+		current = settings.current
+		if settings.enabled:
+			args += ['--mode', '%dx%d' % (current.width, current.height)]
 		else:
 			args.append('--off')
-		if setting.current_r is not None:
-			args += ['--rate', str(setting.current_r)]
-		if setting.other_output is not None:
-			args += ['--%s' % setting.direction, setting.other_output]
-
+		if current.current_r is not None:
+			args += ['--rate', str(current.current_r)]
+		if settings.other_output is not None:
+			args += ['--%s' % settings.direction, settings.other_output]
 	return args
 
-def set_modes(settings):
-	cmd = [xrandr] + settings_to_args(settings)
+def set_modes(output2settings):
+	cmd = [xrandr] + settings_to_args(output2settings)
 	cerr, cin = popen2.popen4(cmd)
 	cin.close()
 	errors = cerr.read()
